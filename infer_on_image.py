3137989bf1a0fd18c27dc96ab279de461afc97ef
BuiKhoi <khoibui2319@gmail.com>
2021-10-09 23:29:28 +0700

    Add image inference codes

---
+++ infer_on_image.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from PIL import Image
+import numpy as np
+from argparse import ArgumentParser
+from trainer import face_learner
+
+from config import get_config
+
+
+def arguments():
+    parser = ArgumentParser()
+
+    parser.add_argument("--model_path", required=True, help="Path to model checkpoint")
+    parser.add_argument("--images_path", required=True, help="Path to images folder")
+    parser.add_argument("--databank", required=True, help="Path to saved databank")
+
+    return parser.parse_args()
+
+
+def main(args):
+    images_path = Path(args.images_path)
+    databank = np.load(args.databank)
+    embeddings = databank["embeddings"]
+    labels = databank["labels"]
+
+    conf = get_config()
+    inferer = face_learner(conf, True)
+    inferer.load_state(conf, args.model_path, False, True)
+
+    files = list(images_path.rglob("*.jpg"))
+    images = [Image.open(str(img)) for img in files]
+    min_idx, _ = inferer.infer(conf, images, embeddings, True)
+
+    for idx, f in zip(min_idx, files):
+        parts = f.parts
+        print(f"Spiece: {parts[-2]}, File: {parts[-1]}, Predicted: {labels[idx]}")
